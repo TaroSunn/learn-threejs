ebe73766eb098dfd8480b16022db0b07ac06b68b
sunxm <sunxm@wanfangdata.com.cn>
2023-06-13 11:06:25 +0800

    feat: Resize

--- src/07 Gasp.ts
+++ src/07 Gasp.ts
@@ -34,7 +34,6 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 // 创建轨道控制器
 const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
 
 // 添加坐标轴辅助器
 const axesHelper = new THREE.AxesHelper(5)
@@ -53,18 +52,7 @@ gsap.to(cube.rotation, {x: 2 * Math.PI, duration: 5, ease: 'power1.inout', repea
 function animation() {
 
   requestAnimationFrame(animation)
-  controls.update()
+
   renderer.render(scene, camera)
 }
 animation()
-
-window.addEventListener('resize', () => {
-  // 更新摄像头
-  camera.aspect = window.innerWidth / window.innerHeight
-  // 更新摄像机的投影矩阵
-  camera.updateProjectionMatrix()
-  // 更新渲染器
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  // 设置渲染器的像素比
-  renderer.setPixelRatio(window.devicePixelRatio)
-})
